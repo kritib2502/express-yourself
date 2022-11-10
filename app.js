7ea1b91a6a72cab7641aff83ad1f60947fccae2b
Kriti <kritib2502@gmail.com>
2022-11-10 14:47:37 -0800

    updated profilesRouter

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ app.listen(port, () => {
   });
   
 const indexRouter = require("./routers/indexRouter");
-
+const profilesRouter = require("./routers/profilesRouter");
 
 // index routes
 app.use(indexRouter);
+app.use(profilesRouter);
 // Morgan Logging Middleware
 const logger = require("morgan");
 // Using logger as middleware, with 3 different output templates
@@ -23,3 +24,15 @@ app.use(logger("dev")); // method, path, status, time
 
 // Express.static middleware to make the public folder globally accessible
 app.use(express.static("public"));
+
+
+
+
+//catch any unmatched routes
+app.all("/*", (req, res) => {
+    res.status(404).send("File Not Found");
+  });
+  
+
+ 
+
