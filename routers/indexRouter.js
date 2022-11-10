ac4ca78151607c14c8178c5fb04aaa84b82c27a1
sara rahmani <sararahmani96@gmail.com>
2022-11-10 15:44:34 -0800

    footer-header-fullwidth-profiles-indexrouter

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -2,8 +2,9 @@
 const express = require("express");
 const indexRouter = express.Router();
 
-indexRouter.get("/", (req, res) => res.send("Hello World!"));
-indexRouter.get("/about", (req, res) => res.send("About Us"));
+ //index
+ indexRouter.get("/", (req, res) => res.send("Welcome Home!"));
+ indexRouter.get("/about", (req, res) => res.send("Out and about in a boat"));
 //indexRouter.get("/contact", (req, res) => res.send("Contact Us"));
 
 //indexRouter.post("/contact", (req, res) => res.send("Thank you contacting"));
