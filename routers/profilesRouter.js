97f6a9d428e03794e395f545beb8c8823a39e851
sara rahmani <sararahmani96@gmail.com>
2022-11-10 15:55:14 -0800

    profilesRouter

--- routers/profilesRouter.js
+++ routers/profilesRouter.js
@@ -4,11 +4,9 @@ const profilesRouter = express.Router();
 
 profilesRouter.get("/profiles", (req,res) => res.send("Profiles List"));
 
-<<<<<<< HEAD
-=======
 profilesRouter.get("/:profileId", (req, res) => {
   res.send(`Individual Prodile ${req.params.profileId}`);
 });
->>>>>>> sara
+
 
 module.exports = profilesRouter;
