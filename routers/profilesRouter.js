71abc85c81aab72d45cd29f7fa532915c890e2cf
Kriti <kritib2502@gmail.com>
2022-11-10 15:02:16 -0800

    update views

--- routers/profilesRouter.js
+++ routers/profilesRouter.js
@@ -4,5 +4,8 @@ const profilesRouter = express.Router();
 
 profilesRouter.get("/profiles", (req,res) => res.send("Profiles List"));
 
+profilesRouter.get("/:profileID", (req,res) => {
+    res.send(`Profile details ${req.params.profileID}`);
+} );
 
 module.exports = profilesRouter;
