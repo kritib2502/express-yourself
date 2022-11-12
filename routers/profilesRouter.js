2e11248f709832248f49c2cda7c35b61331f9bd5
Kriti <kritib2502@gmail.com>
2022-11-12 14:44:26 -0800

    upated routers and views

--- routers/profilesRouter.js
+++ routers/profilesRouter.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const profilesRouter = express.Router();
+const path = require("path");
+const fs = require("fs");
+
+// profilesRouter.get("/:profileId", (req, res) => {
+//   res.render(`Individual Prodile ${req.params.profileId}`);
+// });
 
 
-profilesRouter.get("/profiles", (req,res) => res.send("Profiles List"));
+const dataPath = path.join(__dirname, "../data/");
 
-profilesRouter.get("/:profileId", (req, res) => {
-  res.send(`Individual Prodile ${req.params.profileId}`);
+profilesRouter.get("/profiles", (req, res) => {
+  fs.readFile(dataPath + "profiles.json")
+    .then((contents) => {
+      console.log(contents);
+      // need to parse the raw buffer as json if we want to work with it
+      const profilesJson = JSON.parse(contents);
+      console.log(profilesJson);
+      //   prepare and send an OK response
+      res.render("profiles", {
+        title: "Express Yourself - Profiles",
+        profiles: profilesJson,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.writeHead(500);
+      res.end("Error");
+    });
 });
-
-
 module.exports = profilesRouter;
