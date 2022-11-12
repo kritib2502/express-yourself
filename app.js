1017253e2da1eeefb3b0674abd86adc413a73813
Kriti <kritib2502@gmail.com>
2022-11-12 13:12:17 -0800

    updated things

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
-const port = process.envPORT || 3003;
+const port = process.env.port || 3003;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -17,6 +17,14 @@ const profilesRouter = require("./routers/profilesRouter");
 // index routes
 app.use(indexRouter);
 app.use(profilesRouter);
+
+
+// tell Express where to find our templates (views)
+app.set("views", path.join(__dirname, "views"));
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+
 // Morgan Logging Middleware
 const logger = require("morgan");
 // Using logger as middleware, with 3 different output templates
@@ -26,6 +34,10 @@ app.use(logger("dev")); // method, path, status, time
 app.use(express.static("public"));
 
 
+
+app.use(expressLayouts);
+
+app.set("layout", "./layouts/full-width");
 
 
 
@@ -38,6 +50,3 @@ app.all("/*", (req, res) => {
  
 
 
-app.use(expressLayouts);
-
-app.set("layout", "./layouts/full-width");
